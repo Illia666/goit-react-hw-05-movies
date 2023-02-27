e71fae87ff44fc21907710dd73488f501d18d80e
Illia666 <108007460+Illia666@users.noreply.github.com>
2023-02-27 11:22:00 +0000

    Deploying to gh-pages from @ Illia666/goit-react-hw-05-movies@325d24cd6b27e16297f87f71f948c5011d863926 🚀

---
+++ static/js/983.9eec5a0b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t),r.d(t,{default:function(){return i}});var o=r(4691),a="loader_box__PwN30",n=r(184),i=function(){return(0,n.jsx)("div",{className:a,children:(0,n.jsx)(o.s5,{strokeColor:"#ff0000",strokeWidth:"5",animationDuration:"0.75",width:"96",visible:!0})})}}}]);
+//# sourceMappingURL=983.9eec5a0b.chunk.js.map
